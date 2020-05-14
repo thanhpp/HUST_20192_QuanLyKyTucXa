3e9ee4a8739c368fad20099b83243cd3cc769d35
Trần Đình Vũ <53107088+trandinhvu13@users.noreply.github.com>
2020-05-14 21:57:02 +0700

    Done index cho SV

--- app/frontend/assets/js/custom.js
+++ app/frontend/assets/js/custom.js
@@ -6,3 +6,12 @@
  */
 
 "use strict";
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [ 'dayGrid' ]
+  });
+
+  calendar.render();
+});
