ae836a43899749bf3363fe3a87f91e92a0ca5b33
Trần Đình Vũ <53107088+trandinhvu13@users.noreply.github.com>
2020-06-01 17:13:18 +0700

    Update pics + xong trang gửi xe

--- app/frontend/assets/js/quan-ly-gui-xe.js
+++ app/frontend/assets/js/quan-ly-gui-xe.js
@@ -1,288 +1,76 @@
 var studentsParkingList = [
   {
-    id: 1,
     name: "Trần Đình Vũ",
     student_id: "20176126",
-    room: "201 B6",
-    fee_status: "Đã thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/1/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
+    room: "201",
+    type_of_vehicle: "Xe máy",
+    plate: "1111-1111",
   },
   {
-    id: 2,
     name: "Trần Đình A",
-    student_id: "20176111",
-    room: "201 B6",
-    fee_status: "Đã thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/12/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
+    student_id: "20176122",
+    room: "201",
+    type_of_vehicle: "Xe máy",
+    plate: "1111-1111",
   },
   {
-    id: 3,
     name: "Trần Đình B",
-    student_id: "20176112",
-    room: "201 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
+    student_id: "20175343",
+    room: "202",
+    type_of_vehicle: "Xe đạp",
+    plate: "Không có",
   },
   {
-    id: 4,
     name: "Trần Đình C",
-    student_id: "20176113",
-    room: "203 B6",
-    fee_status: "Đã thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
+    student_id: "201761512",
+    room: "101",
+    type_of_vehicle: "Xe máy",
+    plate: "1111-2221",
   },
   {
-    id: 5,
     name: "Trần Đình D",
-    student_id: "20176133",
-    room: "203 B6",
-    fee_status: "Đã thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
+    student_id: "20176006",
+    room: "201",
+    type_of_vehicle: "Xe đạp điện",
+    plate: "1111-1111",
   },
   {
-    id: 6,
-    name: "Trần Đình E",
-    student_id: "20176144",
-    room: "202 B6",
-    fee_status: "Đã thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
+    name: "Trần Mai Mai",
+    student_id: "20176226",
+    room: "205",
+    type_of_vehicle: "Xe đạp điện",
+    plate: "0931-2341",
   },
   {
-    id: 7,
-    name: "Nguyễn Văn A",
-    student_id: "20176134",
-    room: "202 B6",
-    fee_status: "Chưa thanh toán",
-    register_date: "1/1/2019",
+    name: "Trần Đình Tiến",
+    student_id: "20176516",
+    room: "301",
+    type_of_vehicle: "Xe đạp",
+    plate: "Không có",
   },
   {
-    id: 8,
-    name: "Nguyễn Văn B",
-    student_id: "20176151",
-    room: "201 B6",
-    fee_status: "Đã thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 9,
-    name: "Nguyễn Văn C",
-    student_id: "20176216",
-    room: "202 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 10,
-    name: "Nguyễn Văn D",
-    student_id: "20176335",
-    room: "203 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 11,
-    name: "Nguyễn Văn E",
-    student_id: "20176116",
-    room: "204 B6",
-    fee_status: "Đã thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 12,
-    name: "Nguyễn Văn F",
-    student_id: "20176546",
-    room: "204 B6",
-    fee_status: "Đã thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 13,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 14,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 15,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 16,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 17,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 18,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 19,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 20,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 21,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 22,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
-  },
-  {
-    id: 23,
-    name: "Nguyễn Văn G",
-    student_id: "20176098",
-    room: "204 B6",
-    fee_status: "Chưa thanh toán",
-    update_date: "1/1/2020",
-    address: "Hà Nội",
-    dob: "15/10/1999",
-    contact: "0382212381",
-    register_date: "1/1/2019",
+    name: "Trần Đình Trần",
+    student_id: "20176426",
+    room: "105",
+    type_of_vehicle: "Xe đạp",
+    plate: "Không có",
   },
 ];
 
+for (var i = 0; i < Object.keys(studentsParkingList).length; i++) {
+  studentsParkingList[i].stt = i + 1;
+}
+
 var studentsTable = new Tabulator("#student-parking-table", {
   height: 400, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-  data: studentsList, //assign data to table
+  data: studentsParkingList, //assign data to table
   virtualDom: true,
   layout: "fitColumns", //fit columns to width of table (optional)
   columns: [
     //Define Table Columns
     {
       title: "STT",
-      field: "id",
+      field: "stt",
       width: 80,
       hozAlign: "center",
       sorter: "number",
@@ -290,26 +78,19 @@ var studentsTable = new Tabulator("#student-parking-table", {
     { title: "Họ và tên", field: "name", sorter: "string" },
     { title: "MSSV", field: "student_id", sorter: "number" },
     { title: "Phòng", field: "room", sorter: "string" },
-    { title: "Tình trạng nộp phí", field: "parking_fee_status", sorter: "string" },
+    { title: "Loại xe", field: "type_of_vehicle", sorter: "string" },
+    { title: "Biển xe", field: "plate", sorter: "string" },
     {
       title: "Ngày cập nhật",
       field: "update_date",
-      sorter: "string",
       visible: false,
+      sorter: "string",
     },
     {
       title: "Ngày đăng kí",
       field: "register_date",
-      sorter: "string",
       visible: false,
-    },
-    { title: "Địa chỉ", field: "address", sorter: "string", visible: false },
-    { title: "Ngày sinh", field: "text", sorter: "date", visible: false },
-    {
-      title: "Thông tin liên lạc",
-      field: "contact",
       sorter: "string",
-      visible: false,
     },
   ],
   rowClick: function (e, row) {
@@ -317,19 +98,16 @@ var studentsTable = new Tabulator("#student-parking-table", {
     var name = row.getData().name;
     var student_id = row.getData().student_id;
     var room = row.getData().room;
-    var fee_status = row.getData().fee_status;
+    var type_of_vehicle = row.getData().type_of_vehicle;
     var update_date = row.getData().update_date;
-    var address = row.getData().address;
-    var dob = row.getData().dob;
-    var contact = row.getData().contact;
+    var plate = row.getData().plate;
     var register_date = row.getData().register_date;
     $("#student-table-modal").modal("show");
     $('input[id="student-name"]').val(name);
     $('input[id="student-id"]').val(student_id);
     $('input[id="student-room"]').val(room);
-    $('input[id="dob"]').val(dob);
-    $('input[id="student-address"]').val(address);
-    $('input[id="student-contact"]').val(contact);
+    $('input[id="type_of_vehicle"]').val(type_of_vehicle);
+    $('input[id="plate"]').val(plate);
     $("#update_date").text(update_date);
     $("#register_date").text(register_date);
   },
@@ -372,7 +150,19 @@ document.getElementById("filter-type").addEventListener("change", updateFilter);
 document.getElementById("filter-value").addEventListener("keyup", updateFilter);
 
 document.getElementById("download-xlsx").addEventListener("click", function () {
-  studentsTable.download("xlsx", "danhsachsinhvienKTX.xlsx", {
+  studentsTable.download("xlsx", "danhsachsinhvienguixe.xlsx", {
     sheetName: "Danh sách",
   });
 });
+
+$("#add-student").on("click",function() {
+  $("#add-student-table-modal").modal("show");
+})
+
+$("#change-student-button").click(function() {
+  //gui request;
+})
+
+$("#add-student-button").click(function() {
+  //gui-request;
+})
